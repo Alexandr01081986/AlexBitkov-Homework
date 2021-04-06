59823b77c0cb10070f13ad649b7f83cbbfe2e60f
Alexandr01081986 <bitkov_xgold@mail.ru>
2021-04-06 22:30:20 +0300

    Сделал ДЗ 5

--- src/ru/geekbrains/courses/java/lesson5/Main.java
+++ src/ru/geekbrains/courses/java/lesson5/Main.java
@@ -1,4 +1,20 @@
 package ru.geekbrains.courses.java.lesson5;
 
 public class Main {
+
+    public static void main(String[] args) {
+        Employee[] employees = new Employee[5];
+        employees[0] = new Employee("Ivanov Ivan Ivanovich", "Enginer", "III@mail.ru", "890000000", 30000, 36);
+        employees[1] = new Employee("Smirnov Sergey Sergeevich", "Driver", "SSS@mail.ru", "89111111", 29000, 41);
+        employees[2] = new Employee("Alexeev Alexey Alexeevich", "Manager", "AAA@mail.ru", "8922222", 45000,45);
+        employees[3] = new Employee("Dmitriev Dmitriy Dmitrievich", "Electrician", "DDD@mail.ru", "8933333", 19000, 29);
+        employees[4] = new Employee("Borisov Boris Borisovich", "Accountant", "BBB@mail.ru", "89555555", 39000, 39);
+
+        for (Employee employee : employees) {
+            if (employee.getAge() > 40) {
+                System.out.println(employee.toString());
+            }
+        }
+    }
+
 }
